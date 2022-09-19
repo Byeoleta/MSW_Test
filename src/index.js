a511e47dfead57fecc6848e25fca192408ff7ac9
byeol <jeongeunbyeol8@gmail.com>
2022-09-19 17:11:10 +0900

    서비스 워커 삽입

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { worker } from "./mocks/worker";
+if (process.env.NODE_ENV === "development") {
+  worker.start();
+}
+// 일반적으로 개발환경에서만 가짜 API 사용하므로 환경 변수를 체크하여 선택적으로 서비스 워커 구동
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
