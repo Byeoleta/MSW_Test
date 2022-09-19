883515ad5d91003017fce3ad749841fd5c5e7a90
byeol <jeongeunbyeol8@gmail.com>
2022-09-19 17:39:50 +0900

    테스트 코드 작성

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders todos", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const listitems = await screen.findAllByRole("listitem");
+  expect(listitems).toHaveLength(3);
+
+  userEvent.type(screen.getAllByRole("textbox"), "준비물 챙기기");
+  userEvent.click(screen.getAllByRole("button"));
+
+  expect(await screen.findAllByText("준비물 챙기기")).toBeInTheDocument();
 });
